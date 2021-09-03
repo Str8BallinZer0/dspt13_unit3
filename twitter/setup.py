c91c492f17562a47ff9a590bd0ad9ffa36fcf533
Str8BallinZer0 <derek.l.holtz@gmail.com>
2021-09-02 21:12:20 -0600

    added files to repo

---
+++ twitter/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name='dspt13derekl',
+      version='0.1',
+      description='A foo utility',
+      author='Ewen Cheslack-Postava',
+      author_email='me@ewencp.org',
+      platforms=['any'],  # more specific e.g. 'win32' 'cygwin' 'osx'
+      license='BSD',
+      url='http://github.com/ewencp/foo',
+      packages=find_packages(),
+      )
---
+++ twitter/tests/test_base.py
@@ -0,0 +1,5 @@
+from twitter.twitter import test_function
+
+
+def test_test_function():
+    assert (test_function() == 7)
